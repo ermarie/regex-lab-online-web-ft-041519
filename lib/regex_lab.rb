65b369eea5274d5868740e52472a7856e021ad4f
Erin Stalzer <erinstalzer@gmail.com>
2019-04-19 23:59:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,9 @@
 def starts_with_a_vowel?(word)
-=begin  if word.match(/\A/[aeiou]/)
+  if word.match(/^[aeiou]/)
     true
   else
     false
   end 
-=end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
