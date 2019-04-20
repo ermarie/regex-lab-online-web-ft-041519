37872fda7a4278f2b45e9001983c063206c95b75
Erin Stalzer <erinstalzer@gmail.com>
2019-04-20 00:04:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   array = []
-  if word.match(^un) && word.match()
+  if word.match(/\A/un/) && word.match()
 end
 
 def words_five_letters_long(text)
